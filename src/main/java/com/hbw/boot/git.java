edd981b0a2291be9a447a2c466fd1cba747c93d7
Henry <Henry@qq.com>
2023-09-07 11:39:49 +0800

    test hot-fixmerge commit2

--- src/main/java/com/hbw/boot/git.java
+++ src/main/java/com/hbw/boot/git.java
@@ -4,7 +4,7 @@ public class git {
     public static void main(String[] args) {
         System.out.println("第一个版本");
         System.out.println("第二个版本");
-        System.out.println("第三个版本");
+        System.out.println("第三个版本");//123456
         System.out.println("第四个版本");
         System.out.println("test第个版本");
         System.out.println("test push");
